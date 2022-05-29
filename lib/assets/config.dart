e8084b45cdc54ef70b719493e1ce3a8dd9795295
vignesh-cloud-prog <vigneshun80@gmail.com>
2022-05-29 08:23:21 +0530

    login completed
    Signed-off-by: vignesh-cloud-prog <vigneshun80@gmail.com>

---
+++ lib/assets/config.dart
@@ -0,0 +1,9 @@
+
+   const String appName = "Expertis";
+   const String apiURL = '192.168.124.158:4000'; //PROD_URL
+  //  const String apiURL = 'expertis-api.herokuapp.com'; //PROD_URL
+  //  const String apiURL = '10.0.2.2:4000'; //PROD_URL
+   const String loginAPI = "/users/login";
+   const String registerAPI = "/users/register";
+   const String userProfileAPI = "/users/user-Profile";
+
---
+++ lib/services/api_service.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import '../models/login_request_model.dart';
+import '../models/login_response_model.dart';
+import '../models/register_request_model.dart';
+import '../models/register_response_model.dart';
+import 'package:http/http.dart' as http;
+
+import '../assets/config.dart' as Constants;
+import './user_storage.dart';
+
+class APIService {
+  static var client = http.Client();
+
+  static Future<bool> login(
+    LoginRequestModel model,
+  ) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.http(
+      Constants.apiURL,
+      Constants.loginAPI,
+    );
+
+    var response = await client.post(
+      url,
+      headers: requestHeaders,
+      body: jsonEncode(model.toJson()),
+    );
+
+    if (response.statusCode == 200) {
+      await UserStorage.setUserToken(
+        loginResponseJson(
+          response.body,
+        ),
+      );
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  static Future<RegisterResponseModel> register(
+    RegisterRequestModel model,
+  ) async {
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+    };
+
+    var url = Uri.http(
+      Constants.apiURL,
+      Constants.registerAPI,
+    );
+
+    var response = await client.post(
+      url,
+      headers: requestHeaders,
+      body: jsonEncode(model.toJson()),
+    );
+
+    return registerResponseJson(
+      response.body,
+    );
+  }
+
+  static Future<String> getUserProfile() async {
+    var loginDetails = await UserStorage.getUserToken();
+
+    Map<String, String> requestHeaders = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic ${loginDetails!.data.token}'
+    };
+
+    var url = Uri.http(Constants.apiURL, Constants.userProfileAPI);
+
+    var response = await client.get(
+      url,
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      return "";
+    }
+  }
+}
